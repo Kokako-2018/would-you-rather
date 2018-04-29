f455a61b2dd490f3f1c2f8adf70fd636a7ad3ca2
Rosie Burkett <burkettroz@gmail.com>
2018-04-29 21:44:19 +1200

    fixed knex file again

--- knexfile.js
+++ knexfile.js
@@ -29,14 +29,16 @@ module.exports = {
     }
   },
 
-  client: 'postgresql',
-  connection: process.env.DATABASE_URL,
-  pool: {
-    min: 2,
-    max: 10
-  },
-  migrations: {
-    tableName: 'knex_migrations'
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
   }
 };
 
